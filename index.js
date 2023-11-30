20938280c6225fd0730db3588fbd686929f29fab
Xpronoob <kevin_03_puris@hotmail.com>
2023-11-30 17:44:00 -0600

    Deploy Vercel Config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path, { dirname } from 'path'
 
 const app = express()
 // const PORT = process.env.PORT || 3005
-const PORT = 3005
+const PORT = process.env.port || 3005
 
 // Obtén la ruta del directorio actual
 const __filename = fileURLToPath(import.meta.url)
